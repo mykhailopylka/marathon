065e6c20a46832daa0b5deb7c0fe886f9b582f1c
mykhailopylka <mykhailo.pylka@gmail.com>
2020-03-06 17:21:51 +0100

    menu

--- start/js/main.js
+++ start/js/main.js
@@ -5,4 +5,13 @@ $(function() {
     autoplay: 3000,
     dots: true
   });
+
+  // .addClass - додає функцію, відкриває меню
+  $(".header-btn").on("click", function() {
+    $(".menu").addClass("active");
+  });
+  //  .removeClass - дана функція закриває меню
+  $(".close-btn").on("click", function() {
+    $(".menu").removeClass("active");
+  });
 });
